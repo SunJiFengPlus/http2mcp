a50b62be68d37bdeecca1c3413429bc9ee0d3923
sunjifeng <jifeng.sun@outlook.com>
2025-08-25 15:19:36 +0800

    补充mcp的demo

--- src/main/java/org/apache/camel/examples/model/HttpRequestBean.java
+++ src/main/java/org/apache/camel/examples/model/HttpRequestBean.java
@@ -1,20 +1,27 @@
 package org.apache.camel.examples.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.HashMap;
 
 @Data
+@AllArgsConstructor
 public class HttpRequestBean {
-    private String method = "GET";
+    private String method;
     private String url;
-    private Map<String, String> headers = new HashMap<>();
+    private Map<String, String> headers;
     private String body;
-    private Map<String, String> queryParams = new HashMap<>();
+    private Map<String, String> queryParams;
     
     public String buildFullUrl() {
+        if (Objects.isNull(queryParams)) {
+            queryParams = new HashMap<>();
+        }
+        
         // 不是http param, 是camel内置参数, false时非200不再抛出异常
         queryParams.put("throwExceptionOnFailure", "false");
 
